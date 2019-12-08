5e67a31ca8fdf086e31d54a9678218bc17ae080d
Elias Farhan <elias.farhan@gmail.com>
2019-12-08 14:00:00 +0100

    Remove USE_SDL macros

--- src/input.cpp
+++ src/input.cpp
@@ -1,43 +1,6 @@
 #include <input.h>
 #include <engine.h>
 
-#ifdef USE_SFML2
-InputManager::InputManager()
-{
-	previousButtonStatus.resize(sf::Keyboard::Key::KeyCount);
-}
-
-bool InputManager::GetButtonDown(sf::Keyboard::Key key)
-{
-	return !previousButtonStatus[key] && sf::Keyboard::isKeyPressed(key);
-}
-
-bool InputManager::GetButtonUp(sf::Keyboard::Key key)
-{
-	return previousButtonStatus[key] && !sf::Keyboard::isKeyPressed(key);
-}
-
-bool InputManager::GetButton(sf::Keyboard::Key key)
-{
-	return sf::Keyboard::isKeyPressed(key);
-}
-
-Vec2f InputManager::GetMousePosition()
-{
-	Engine* engine = Engine::GetPtr();
-	auto* window = engine->GetWindow();
-	return static_cast<sf::Vector2f>(sf::Mouse::getPosition(*window));
-}
-
-void InputManager::Update()
-{
-	for (int i = 0; i < sf::Keyboard::Key::KeyCount; i++)
-	{
-		previousButtonStatus[i] = sf::Keyboard::isKeyPressed(static_cast<sf::Keyboard::Key>(i));
-	}
-}
-#endif
-#ifdef USE_SDL2
 InputManager::InputManager()
 {
 }
@@ -62,7 +25,6 @@ Vec2f InputManager::GetMousePosition()
 	SDL_GetMouseState(&x, &y);
 	return Vec2f(x, y);
 }
-#endif
 float InputManager::GetMouseWheelDelta()
 {
 	return wheelDelta;
